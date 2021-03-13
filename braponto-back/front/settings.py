f71f686da7d440db483cccf3bc55f8e50c5e3346
Rodrigo Braga <rodrigo.bragaaa@gmail.com>
2021-03-12 20:12:36 -0400

    Correção bug commit

--- braponto-back/front/settings.py
+++ braponto-back/front/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
 #CORS_ALLOW_CREDENTIALS = True
 #CORS_ORIGIN_WHITELIST = [
